1128c622fe633b237da14ef91036bd2c26bfc177
Yuji Ueki <unhappychoice@gmail.com>
2017-07-27 01:23:45 +0900

    :+1: Implement some private APIs

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -18,6 +18,34 @@ module Bitflyer
           @connection.get('/v1/me/getcollateral').body
         end
 
+        def addresses
+          @connection.get('/v1/me/getaddresses').body
+        end
+
+        def coin_ins
+          @connection.get('/v1/me/getcoinins').body
+        end
+
+        def coin_outs
+          @connection.get('/v1/me/getcoinouts').body
+        end
+
+        def bank_accounts
+          @connection.get('/v1/me/getbankaccounts').body
+        end
+
+        def deposits
+          @connection.get('/v1/me/getdeposits').body
+        end
+
+        def withdraw
+          # TBD
+        end
+
+        def withdrawals
+          @connection.get('/v1/me/getwithdrawals').body
+        end
+
         def positions
           @connection.get('/v1/me/getpositions', { product_code: 'FX_BTC_JPY' }).body
         end
