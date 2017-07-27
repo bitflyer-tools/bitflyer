c0f3c2a86b82425c01d7c3c94bc68daf725de1db
Yuji Ueki <unhappychoice@gmail.com>
2017-07-27 18:27:31 +0900

    :+1: Implement all of private APIs

--- lib/bitflyer/http/private.rb
+++ lib/bitflyer/http/private.rb
@@ -38,16 +38,31 @@ module Bitflyer
           @connection.get('/v1/me/getdeposits').body
         end
 
-        def withdraw
-          # TBD
+        def withdraw(currency_code: 'JPY', bank_account_id: nil, amount: nil, code: nil)
+          body = {
+              currency_code: currency_code,
+              bank_account_id: bank_account_id,
+              amount: amount,
+              code: code
+          }.delete_if { |_, v| v.nil? }
+          @connection.post('/v1/me/withdraw', body).body
         end
 
         def withdrawals
           @connection.get('/v1/me/getwithdrawals').body
         end
 
-        def send_child_order
-          #TBD
+        def send_child_order(product_code: 'BTC_JPY', child_order_type: nil, side: nil, price: nil, size: nil, minute_to_expire: nil, time_in_force: 'GTC')
+          body = {
+              product_code: product_code,
+              child_order_type: child_order_type,
+              side: side,
+              price: price,
+              size: size,
+              minute_to_expire: minute_to_expire,
+              time_in_force: time_in_force
+          }.delete_if { |_, v| v.nil? }
+          @connection.post('/v1/me/sendchildorder', body).body
         end
 
         def cancel_child_order(product_code: 'BTC_JPY', child_order_id: nil, child_order_acceptance_id: nil)
@@ -56,11 +71,17 @@ module Bitflyer
               child_order_id: child_order_id,
               child_order_acceptance_id: child_order_acceptance_id
           }.delete_if { |_, v| v.nil? }
-          @connection.post('/v1/me/cancelchildorder', body)
+          @connection.post('/v1/me/cancelchildorder', body).body
         end
 
-        def send_parent_order
-          #TBD
+        def send_parent_order(order_method: nil, minute_to_expire: nil, time_in_force: 'GTC', parameters: parameters)
+          body = {
+              order_method: order_method,
+              minute_to_expire: minute_to_expire,
+              time_in_force: time_in_force,
+              parameters: parameters
+          }.delete_if { |_, v| v.nil? }
+          @connection.post('/v1/me/sendparentorder', body).body
         end
 
         def cancel_parent_order(product_code: 'BTC_JPY', parent_order_id: nil, parent_order_acceptance_id: nil)
@@ -69,7 +90,7 @@ module Bitflyer
               parent_order_id: parent_order_id,
               parent_order_acceptance_id: parent_order_acceptance_id
           }.delete_if { |_, v| v.nil? }
-          @connection.post('/v1/me/cancelparentorder', body)
+          @connection.post('/v1/me/cancelparentorder', body).body
         end
 
         def cancel_all_child_orders(product_code: 'BTC_JPY')
