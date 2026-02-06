9395a17597f847df4b250638a2fd0c7344a5e6d3
Yuji Ueki <unhappychoice@gmail.com>
2026-02-07 00:06:39 +0900

    refactor: replace faraday with net/http standard library (#651)
    - Replace Faraday::Connection with Net::HTTP-based Connection class
    - Remove Faraday::Middleware and inline authentication logic
    - Add explicit require 'forwardable' in realtime/client.rb (previously loaded transitively by faraday)
    - Remove faraday dependency from gemspec
    
    Closes #4

--- lib/bitflyer/http.rb
+++ lib/bitflyer/http.rb
@@ -3,45 +3,62 @@
 require 'bitflyer'
 require 'bitflyer/http/public'
 require 'bitflyer/http/private'
-require 'faraday'
+require 'net/http'
+require 'uri'
+require 'json'
 require 'openssl'
 
 module Bitflyer
   module HTTP
+    BASE_URL = 'https://api.bitflyer.jp'
+
+    Response = Struct.new(:body)
+
     class Connection
-      extend Forwardable
-
-      def_delegators :@connection, :get, :post
-
       def initialize(key, secret)
-        @connection = Faraday::Connection.new(url: 'https://api.bitflyer.jp') do |f|
-          f.request :json
-          f.response :json
-          f.use Authentication, key, secret
-          f.adapter Faraday.default_adapter
-        end
-      end
-    end
-
-    class Authentication < Faraday::Middleware
-      def initialize(app, key, secret)
-        super(app)
         @key = key
         @secret = secret
+        @uri = URI.parse(BASE_URL)
       end
 
-      def call(env)
-        return @app.call(env) if @key.nil? || @secret.nil?
+      def get(path, params = {})
+        query = URI.encode_www_form(params.compact)
+        full_path = query.empty? ? path : "#{path}?#{query}"
+        request = Net::HTTP::Get.new(full_path, default_headers)
+        sign_request(request, full_path)
+        Response.new(execute(request))
+      end
+
+      def post(path, body = {})
+        request = Net::HTTP::Post.new(path, default_headers.merge('Content-Type' => 'application/json'))
+        request.body = JSON.generate(body.compact)
+        sign_request(request, path, request.body)
+        Response.new(execute(request))
+      end
+
+      private
+
+      def default_headers
+        { 'Accept' => 'application/json' }
+      end
+
+      def sign_request(request, path, body = '')
+        return if @key.nil? || @secret.nil?
 
         timestamp = Time.now.to_i.to_s
-        method = env[:method].to_s.upcase
-        path = env[:url].path + (env[:url].query ? "?#{env[:url].query}" : '')
-        body = env[:body] || ''
-        signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), @secret, timestamp + method + path + body)
-        env[:request_headers]['ACCESS-KEY'] = @key if @key
-        env[:request_headers]['ACCESS-TIMESTAMP'] = timestamp
-        env[:request_headers]['ACCESS-SIGN'] = signature
-        @app.call env
+        method = request.method.upcase
+        text = timestamp + method + path + (body || '')
+        signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), @secret, text)
+        request['ACCESS-KEY'] = @key
+        request['ACCESS-TIMESTAMP'] = timestamp
+        request['ACCESS-SIGN'] = signature
+      end
+
+      def execute(request)
+        response = Net::HTTP.start(@uri.host, @uri.port, use_ssl: true) { |http| http.request(request) }
+        JSON.parse(response.body)
+      rescue JSON::ParserError
+        response.body
       end
     end
   end
--- lib/bitflyer/realtime/client.rb
+++ lib/bitflyer/realtime/client.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'forwardable'
 require_relative 'websocket'
 
 module Bitflyer
